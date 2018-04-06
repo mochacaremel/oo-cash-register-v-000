ca6c850401268c71973bda95016be238094f6238
Asheley McDonald <asheleymcdonald@gmail.com>
2018-04-06 13:31:28 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,46 @@
+require "pry"
+class CashRegister
+  attr_accessor :total, :discount
 
+  @@items = []
+  @@last_item = []
+
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @@items.clear
+  end
+
+  def discount
+    @discount
+  end
+
+  def add_item(title, price, quantity=1)
+    @total += (price * quantity)
+    @@last_item[0], @@last_item[1], @@last_item[2] = title, price, quantity
+    while quantity > 0
+      @@items << title
+      quantity -= 1
+    end
+  end
+
+  def apply_discount
+    if @discount == 0
+      "There is no discount to apply."
+    else
+      @total -= (@total * (@discount / 100.to_f))
+      "After the discount, the total comes to $#{@total.to_i}."
+    end
+  end
+
+  def items
+    @@items
+  end
+
+  def void_last_transaction
+    price, quantity = @@last_item[1], price = @@last_item[2]
+    @total -= (price * quantity)
+    @@last_item.clear
+    @@items.slice!(-1,quantity)
+  end
+end
